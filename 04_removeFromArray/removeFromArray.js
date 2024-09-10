8ba20bce5f201fddd16653ab6de2603eea76d8f8
Jonah <jonahkf@gmail.com>
2024-09-10 10:32:51 +0900

    COMPLETE: Javascript Exercise 04
    Googled how to use .include() array method.
    Used // Pass; comment inside empty if statement.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...vars) {
+  let newArray = [];
+  for (i = 0; i < array.length; i++) {
+    if (vars.includes(array[i])) {
+      // pass;
+    } else {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
 };
+
+// OLD CODE
+// const removeFromArray = function (array, removeTarget) {
+//     let newArray = [];
+//     for (i = 0; i < array.length; i++) {
+//       if (array[i] != removeTarget) {
+//         newArray.push(array[i]);
+//       }
+//     }
+//     return newArray;
+//   };
 
 // Do not edit below this line
 module.exports = removeFromArray;

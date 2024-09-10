aaa55f4e8ae941e1d6ff20a1d293d92690ebe8e1
Jonah <jonahkf@gmail.com>
2024-09-10 12:42:00 +0900

    COMPELTE: Javascript Exercise 05
    if statements I used can be simplified like solution.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function (firstInt, secondInt) {
+  let result = 0;
+  if (firstInt < 1 || secondInt < 1) {
+    return "ERROR";
+  } else if (Number.isInteger(firstInt) && Number.isInteger(secondInt)) {
+    if (firstInt < secondInt) {
+      //Runs if secondInt is larger
+      for (i = firstInt; i <= secondInt; i++) {
+        result += i;
+      }
+    } else {
+      //Runs if firstInt is larger
+      for (i = secondInt; i <= firstInt; i++) {
+        result += i;
+      }
+    }
+    return result;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line

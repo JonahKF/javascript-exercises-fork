e93c849fede8bef07a6d327cb2da478b3231c4f4
Jonah <jonahkf@gmail.com>
2024-09-18 09:37:18 +0900

    ADDED: Functionality for tests 1 & 6
    To-do: Punctuation, lowercase, spaces.
    Can probably simplify with array methods.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const arr = str.split(""); // 'racecar' -> ['r', 'a', 'c', 'e', 'c', 'a', 'r']
+    
+    let reverse = arr.length;
+    for(let e = 0; e < arr.length; e++){
+        if (arr[e] != arr[reverse - 1]){
+            return false;
+        }
+        reverse--;
+    }
+    return true;
 };
 
 // Do not edit below this line

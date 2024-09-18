6e5a172c335e4595bb6b18e7994e38e8c8d69440
Jonah <jonahkf@gmail.com>
2024-09-18 10:31:53 +0900

    COMPLETE: Javascript Exercise 10
    Used a for loop to iterate through series, while
    keeping track of previous numbers by pushing them
    into an empty array.
    Several if statements used for initial 0 and
    checking strings & positives. This could probably
    be simplified further.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,32 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    //In this exercise, the Fibonacci sequence used is 1, 1, 2, 3, 5, 8, etc.
+    let fib = 1;
+    let arr = [];
+    num = Number(num);
 
+    if(num === 0){
+        return 0;
+    }
+    else if(num < 0){
+        return "OOPS";
+    }
+    else{
+        for(let e = 0; e < num - 1; e++){
+            arr.push(fib);
+            if(e === 0){
+                fib = 1;
+            }
+            else {
+                fib = arr[e] + arr[e - 1];
+            }
+            // console.log("Fib is " + fib)
+            // console.log(arr)
+        }
+    
+        return fib;
+    }
+
+    
 };
 
 // Do not edit below this line

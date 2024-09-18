4eff814192ba33368dd7810ac4f86926564b77bd
Jonah <jonahkf@gmail.com>
2024-09-18 09:53:29 +0900

    COMPLETE: Javascript Exercise 09
    Had to Google regex values for filtering punctuation.
    Final solution uses a For loop, which can probably
    be simplified. Will look at solution for study.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,9 +1,12 @@
 const palindromes = function (str) {
-    const arr = str.split(""); // 'racecar' -> ['r', 'a', 'c', 'e', 'c', 'a', 'r']
+    const arr = str.toLowerCase().split(""); // 'racecar' -> ['r', 'a', 'c', 'e', 'c', 'a', 'r']
+    let newArr = arr.filter(str => !str.match(/[^\w\s]/g));
+    newArr = newArr.filter(str => str != " ")
+            .filter(num => typeof num !== 'number');
     
-    let reverse = arr.length;
-    for(let e = 0; e < arr.length; e++){
-        if (arr[e] != arr[reverse - 1]){
+    let reverse = newArr.length;
+    for(let e = 0; e < newArr.length; e++){
+        if (newArr[e] != newArr[reverse - 1]){
             return false;
         }
         reverse--;

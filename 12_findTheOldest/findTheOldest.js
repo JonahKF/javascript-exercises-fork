280932c88f9e50696704a01d99804acc1f090fc3
Jonah <jonahkf@gmail.com>
2024-09-18 11:22:30 +0900

    COMPLETE: Javascript Exercise 12
    Used a sort() method to place oldest object
    in front, then returned ordered[0].
    Used ternary operator ? to add current year if
    no death year provided in object.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const ordered = arr.sort((a, b) => (a.yearOfDeath === undefined ? new Date().getFullYear() - a.yearOfBirth : a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath === undefined ? new Date().getFullYear() - b.yearOfBirth : b.yearOfDeath - b.yearOfBirth) ? -1 : 1);
+    console.table(ordered);
+    return ordered[0];
 };
 
 // Do not edit below this line
